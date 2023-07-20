be0a9c49a6e6354a6baecfb96d0efa669e08e04b
Ardiy <you@example.com>
2023-07-20 16:29:27 +0000

    stff

---
+++ 0x01-shell_permissions/README.md
@@ -0,0 +1,1 @@
+changing and manipulating user permissions
---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char country[20];
+
+    printf("What country are you from : ");
+    scanf("%s",country);
+
+    printf("oh so youre from %s",country);
+    return 0;
+   
+}
