406b9d4a0758b96e1aea1272ed13fde62771488e
Ardiy <you@example.com>
2023-09-22 23:27:38 +0000

    CM with puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # kill process killmenow
 
-exec { 'killmenow':
-  command     => 'pkill -f "killmenow"',
-  path        => '/usr/bin:/bin',
-
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
